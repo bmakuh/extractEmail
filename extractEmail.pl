b13902bc9dc5c5dbdbe7834b92974ecda3a0ff8a
Ben Makuh <alphari@gmail.com>
2011-05-05 12:00:27 -0600

    that's all folks

--- extractEmail.pl
+++ extractEmail.pl
@@ -10,18 +10,42 @@ sub extractEmail {
 	closedir(DIR);
 	
 	$counter = 0;
+	$num;
 	foreach $file (@files) {
 		if (-f "$dir/$file" and $file =~ /^SIGCSE\.$date/) {
-			print "Searching for topic: $topic in $date\n";
+			print "Searching for topic: $topic\n";
 			open(FILE, "$dir/$file") or die "error opening file! $!\n";
-			if (@results = grep /$search/, <FILE>) {
+			if (@results = grep /$topic/, <FILE>) {
 				print "Topic found in: $file\n";
 				foreach $result (@results) {
-					if ($result =~ /^Subject:/) {
-						print $result;
+					if ($result =~ /[1-9]\./) {
+						print $result . "\n";
+						$num = grep /[1-9]/, $result;
 					}
 				}
 			}
+			close(FILE);
+			open(FILE, "$dir/$file") or die "all ur base r belong to us lolz \n";
+			$count = 0;
+			$println = 0;
+			while ($line = <FILE> and $println != 2) {
+				if ($line =~ /\-{70}/) {
+					$count++;
+				}
+				
+				if ($count == $num) {
+					$println = 1;
+				}
+				
+				if ($println) {
+					print $line;
+				}
+				
+				if ($line =~ /^\-{30}$/) {
+					$println = 2;
+				}
+			}
+			
 			close(FILE);
 		}
 	}
